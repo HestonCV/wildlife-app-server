6defbb5cd6502ea6a4bbf4c547cdc1da8a3b850e
Heston Vaughan <heston.cv@gmail.com>
2023-08-16 03:34:41 -0500

    remove test script

--- test.py
+++
@@ -1,6 +0,0 @@
-from models import db, User, Camera, Image
-
-users = db.session.query(Image).all()
-
-for user in users:
-    print(user.id, user.first_name, user.last_name, user.email, user.camera_id)
