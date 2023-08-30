5d78cf76c795dd5942b229189d8a2e82aeb4f2f1
Heston Vaughan <heston.cv@gmail.com>
2023-08-30 04:20:42 -0500

    Refactor code to include extensive error handling, user register and login, and class/camera image GETs

--- models.py
+++ models.py
@@ -4,22 +4,24 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), nullable=False)
-    password = db.Column(db.String(120), nullable=False)
-    first_name = db.Column(db.String(50), nullable=False)
-    last_name = db.Column(db.String(50), nullable=False)
-    cameras = db.relationship('Camera', backref='user')
-
-
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=False)
+    cameras = db.relationship('Camera', backref=('user'))
+  
 class Camera(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(50), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    description = db.Column(db.String(500))
+    token = db.Column(db.String(256), unique=True)
     images = db.relationship('Image', backref='camera')
 
 class Image(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    filename = db.Column(db.String(100), nullable=False)
-    classification = db.Column(db.String(50), nullable=False)
-    date = db.Column(db.DateTime, nullable=False)
     camera_id = db.Column(db.Integer, db.ForeignKey('camera.id'), nullable=False)
+    file_name = db.Column(db.String(250), nullable=False, unique=True)
+    classification = db.Column(db.String(50), nullable=False)
+    date_time = db.Column(db.DateTime, nullable=False)
+
